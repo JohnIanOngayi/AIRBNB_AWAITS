1a3d3b167d791517d339e0b94904b246bbe979d0
JohnIanOngayi <ongayijohnian@gmail.com>
2024-05-24 20:04:27 +0300

    Playing with utils.abort

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from fabric.api import cd, run, env, task
+from fabric.utils import abort
 
 env.hosts = ['local-web-01', 'local-web-02']
 
@@ -9,3 +10,6 @@ env.hosts = ['local-web-01', 'local-web-02']
 def seeNginx():
     with cd('/etc/nginx/sites-enabled/'):
         run('cat default')
+        run('echo "before abort"')
+        abort('yeeeh budddddyyy!!!')
+        run('echo "after abort"')
